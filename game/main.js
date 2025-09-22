e0ab5008ef5b3165b2502fb2f183ce1568cc172e
Aiden1841 <dzc-650-mjc@e-net.nara.jp>
2025-09-22 06:14:45 +0000

    球を撃つ

--- game/main.js
+++ game/main.js
@@ -1,38 +1,84 @@
-document.getElementById("txt").innerText= "これはゲームです";
 const canvas =document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-//fillRect(x座標(横),y座標(縦),横幅,縦幅)
 
+ const player ={
+    x:canvas.width/2-15,
+    y:canvas.height-60,
+    width:30,
+    height:30,
+  color:"blue",
+    life:5,
+ };
+
+const bullets=[];
+const BULLET_SPEED=-10;
+
+function tryshoot(){
+    bullets.push({
+    x: player.x,
+    y: player.y,
+    width:5,
+    height:5,
+    vy:BULLET_SPEED,
+    })
+}
+
+
+ //fillRect(x座標(横),y座標(縦),横幅,縦幅)
 let x =225;
 
-window.addEventListener("keydown",(e)=>{
+
+window.addEventListener("keydown",(e) => {
 if (e.key==="ArrowLeft"){
-x -= 10;
-}else if (e.key==="ArrowRight"){
-x+=10;
-}else if(e.key==="space"){
-    tama+=1;
+if(player.x>10){
+    player.x -= 10;
 }
+}else if (e.key==="ArrowRight"){
+  if(player.x>5){
+    player.x+= 10;
+}
+}else if (e.key  ==="ArrowUp"){
+  player.y-= 10;
+}else if (e.key  ==="ArrowDown"){
+  player.y+= 10;
+}else if(e.code==="Space"){
+    tryshoot();
+   }
 });
 
-let y1=0;
-let y2=0;
-
-
-function gameLoop() {
+function update(){
+for (let i =0;i<bullets.length;i++){
+   const bullet=bullets[i];
+bullet.y+=bullet.vy;
+     if (bullet.y<0){
+        bullets.splice(i,1);
+     }
+    }
+  }
+function darw(){
     ctx.fillStyle="black";
     ctx.fillRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle="red";
-    ctx.fillRect(150,y1,30,30);
-    y1+=1;
-    ctx.fillStyle="red";
-    ctx.fillRect(300,y2,30,30);
-    y2+=1;
-    ctx.fillStyle = "blue";
-   ctx.fillRect(x,480,30,30);
   
-   requestAnimationFrame(gameLoop);
+    ctx.fillStyle = player.color;
+   ctx.fillRect(player.x,player.y,player.width,player.height);
+  
+   ctx.fillStyle="white";
+for (let i =0;i<bullets.length;i++){
+   const bullet=bullets[i];
+   ctx.fillRect(bullet.x,bullet.y,bullet.width,bullet.height);
+
+}
 }
 
+  function gameLoop(){
+  update();
+darw();
+
+requestAnimationFrame(gameLoop);
+
+ }
 gameLoop();
+
+
+ 
